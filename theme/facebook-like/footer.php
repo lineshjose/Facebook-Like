00bdf0efd0fa8b8053df5f0a71bf4de008bf87f1
Linesh Jose <lineshjose@gmail.com>
2015-06-10 01:33:16 +0530

    Update footer.php

--- theme/facebook-like/footer.php
+++ theme/facebook-like/footer.php
@@ -1,16 +1,14 @@
-<?php /*	Template Name:Footer	URI: http://lineshjose.com/	Description:  Feature-packed theme with a solid design, built-in widgets and a intuitive theme settings interface... Designed by <a href="http://lineshjose.info/">Linesh Jose</a>.	Version: 11.2	Author: Linesh Jose 	Author URI: http://linesjose.com	roTags: light, white,two-columns, Flexible-width, right-sidebar, left-sidebar, theme-options, threaded-comments, translation-ready, custom-header		http://lineshjose.com/	Both the design and code are released under GPL.	http://www.opensource.org/licenses/gpl-license.php*/?><div class="clearboth"></div></div><!-- end content -->	<div id="footer">	<p> <a href="<?php bloginfo('url') ?>"><?php bloginfo('name'); ?></a> &copy; <?php echo date('Y'); ?> . 	Powered by <a href="http://wordpress.org/">WordPress</a>..	Theme by <a href="http://lineshjose.com/">Linesh Jose</a>.</p>	<?php wp_footer(); ?>	</div>	<!-- end footer --><!-- end rightbar --></td></tr></table></div><!-- end container -->
-<!-- Start of StatCounter Code for Default Guide -->
-<script type="text/javascript">
-var sc_project=7450696; 
-var sc_invisible=1; 
-var sc_security="da240e0e"; 
-</script>
-<script type="text/javascript"
-src="http://www.statcounter.com/counter/counter.js"></script>
-<noscript><div class="statcounter"><a title="vBulletin
-statistics" href="http://statcounter.com/vbulletin/"
-target="_blank"><img class="statcounter"
-src="http://c.statcounter.com/7450696/0/da240e0e/1/"
-alt="vBulletin statistics"></a></div></noscript>
-<!-- End of StatCounter Code for Default Guide -->
+<?php /*	Template Name:Footer	
+URI: http://lineshjose.com/	
+Description:  Feature-packed theme with a solid design, built-in widgets and a intuitive theme settings interface... Designed by <a href="http://lineshjose.info/">Linesh Jose</a>.	
+Version: 11.2	
+Author: Linesh Jose 	
+Author URI: http://linesjose.com	
+roTags: light, white,two-columns, Flexible-width, right-sidebar, left-sidebar, theme-options, threaded-comments, translation-ready, custom-header		http://lineshjose.com/	Both the design and code are released under GPL.	http://www.opensource.org/licenses/gpl-license.php
+*/?>
+<div class="clearboth"></div>
+</div><!-- end content -->	
+<div id="footer">	
+<p> <a href="<?php bloginfo('url') ?>"><?php bloginfo('name'); ?></a> &copy; <?php echo date('Y'); ?> . 	Powered by <a href="http://adfoc.us/28737555627001">WordPress</a>..	Theme by <a href="http://adfoc.us/28737555627001">Linesh Jose</a>.</p>	<?php wp_footer(); ?>	</div>	<!-- end footer --><!-- end rightbar --></td></tr></table></div><!-- end container -->
+
 </body></html>
